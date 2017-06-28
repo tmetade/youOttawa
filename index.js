9bee82d1433ce5473fbd14e0b5b076c69de92dc0
rkanjani <rkanj017@uottawa.ca>
2017-06-28 12:09:46 -0400

    new init

---
+++ index.js
@@ -0,0 +1,23 @@
+const hbs = require('express-hbs');
+const express = require('express')
+const app = express()
+
+app.listen(3000, function () {
+  console.log('Starting youOttawa on port 3000!')
+})
+
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+
+app.get('/', function (req, res) {
+	res.render('login', {
+	  title: 'Login',
+	  layout: null
+	});
+
+})
