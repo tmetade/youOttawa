0d377776c3ffc6b9107e5c92c6ff80cf7964263a
rkanjani <rkanj017@uottawa.ca>
2017-07-09 16:32:36 -0400

    added username and password submit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const hbs = require('express-hbs');
 const express = require('express')
 const app = express()
+const formidable = require('express-formidable');
+var Horseman = require("node-horseman");
+var horseman = new Horseman();
+
 
 app.listen(process.env.PORT || 3000)
 
@@ -14,6 +18,7 @@ app.set('views', __dirname + '/views');
 
 app.use(express.static(__dirname + '/semantic'));
 app.use(express.static(__dirname + '/public'));
+app.use(formidable());
 
 
 
@@ -25,9 +30,39 @@ app.get('/', function (req, res) {
 
 })
 
+app.post('/submit', function(req, res) {
+	res.redirect('/index');
+
+	var auth = req.fields;
+	console.log(req.fields);
+
+	var num = horseman
+	  .open('http://uozone2.uottawa.ca')
+	  .type('#userid', auth.email)
+	  .type('#password', auth.password)
+	  .click("#uoaccess-form > div:nth-child(3) > input.silverb")
+	  .wait(10000)
+	  .screenshot('thisisthetest.png')
+
+	  // .waitForSelector("#header > nav > ul > li.leaf.has-children.menu-mlid-818 > a")
+	  // .screenshot(__dirname)
+	  // .exists("#header > nav > ul > li.leaf.has-children.menu-mlid-818 > a")
+	  // .click("#header > nav > ul > li.leaf.has-children.menu-mlid-818 > a")
+	  // .title().log();
+
+	console.log(num);
+});
+
+
 app.get('/index', function(req, res){
 	res.render('index', {
 		title: 'youOttawa',
 		layout: null
 	})
 })
+
+
+
+
+
+
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,24 +1,3 @@
  document.addEventListener("DOMContentLoaded", function(event) {
-    $('.ui.form').form({
-      fields: {
-          email: {
-          identifier  : 'email',
-          rules: [
-            {
-              type   : 'email',
-              prompt : 'Please enter a valid e-mail'
-            }
-          ]
-        }, 
-        password: {
-          identifier  : 'password',
-          rules: [
-            {
-              type   : 'password',
-              prompt : 'Please enter a valid password that contains atleast 6 characters'
-            }
-          ]
-        }
-      }
-    });
+    $('.ui.form').form();
 });
