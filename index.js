0788759850464126befca561e7d05eed391e09d5
rkanjani <rkanj017@uottawa.ca>
2017-07-09 16:47:23 -0400

    brightspace instead of ozone

--- index.js
+++ index.js
@@ -37,12 +37,21 @@ app.post('/submit', function(req, res) {
 	console.log(req.fields);
 
 	var num = horseman
-	  .open('http://uozone2.uottawa.ca')
-	  .type('#userid', auth.email)
-	  .type('#password', auth.password)
-	  .click("#uoaccess-form > div:nth-child(3) > input.silverb")
-	  .wait(10000)
-	  .screenshot('thisisthetest.png')
+		.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36")
+		.open("https://idp3.uottawa.ca/idp/login.jsp?actionUrl=%2Fidp%2FAuthn%2FUserPassword")
+		.type("#username", auth.email)
+		.type("#password", auth.password)
+		.click("body > article > form > div > div:nth-child(4) > button")
+		.wait(5000)
+		.screenshot("testing.png")
+
+
+	  // .open('http://uozone2.uottawa.ca')
+	  // .type('#userid', auth.email)
+	  // .type('#password', auth.password)
+	  // .click("#uoaccess-form > div:nth-child(3) > input.silverb")
+	  // .wait(10000)
+	  // .screenshot('thisisthetest.png')
 
 	  // .waitForSelector("#header > nav > ul > li.leaf.has-children.menu-mlid-818 > a")
 	  // .screenshot(__dirname)
