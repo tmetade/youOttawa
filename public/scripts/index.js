607ab283b50960ce4c22112be1236395714aaf1b
rkanjani <rkanj017@uottawa.ca>
2017-07-12 15:18:24 -0400

    aaaaaa

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,20 +1,34 @@
 var youottawa = angular.module('youottawa', []);
 
+youottawa.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('%%');
+  $interpolateProvider.endSymbol('%%');
+});
 // Define the `PhoneListController` controller on the `phonecatApp` module
-youottawa.controller('MainController', function MainController($scope) {
+youottawa.controller('mainController', ["$scope", function MainController($scope) {
 
 	$scope.keyLinks = [{
 			"title": "Brightspace",
 			"description": "New Blackboard Learn",
 			"url" : "http://uottawa.brightspace.com"	
-		}];
+	}];
+
+	$scope.addLink = function(index){
+		var newLink = {
+			"title": "UoZone",
+			"description": "uOZone",
+			"url" : "http://uozone.uottawa.ca"	
+		}
+		$scope.keyLinks.push(newLink);
+		$scope.$apply();
+	}
 
 
 
-});
+}]);
 
 youottawa.component('keyList', {
-	template: '<div ng-repeat="link in $ctrl.keyLinks" class="ui segment application" data-content="{{link.description}}" href="{{url}}">' + 
+	template: '<div ng-repeat="link in $ctrl.keyLinks" class="ui segment " data-content="{{link.description}}" href="{{url}}">' + 
 				    '<div class="content" >'+
 				     '<a class="header" href="{{link.url}}">{{link.title}}</a>'+
 				       ' </div>'+
