1055ef5406ba992497e483ad97b368c86d13925e
rkanjani <rkanj017@uottawa.ca>
2017-07-13 01:01:38 -0400

    added gmail table, fixed profile

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -12,36 +12,30 @@ youottawa.controller('mainController', ["$scope", "$http", function MainControll
 	    $scope.applications = data.data;
 	  });
 
-	$scope.keyLinks = [{
-			"title": "Brightspace",
-			"description": "New Blackboard Learn",
-			"url" : "http://uottawa.brightspace.com"	
-	}];
+	$scope.keyLinks = [];
 
 
 	$scope.addLink = function(index){
-		console.log($scope.applications)
-		$scope.keyLinks.push($scope.applications[index]);
+		var found = false;
+
+		for(link in $scope.keyLinks){
+			console.log("does it match: " + $scope.keyLinks[link].title + "    " + $scope.applications[index].title);
+			if($scope.keyLinks[link].title == $scope.applications[index].title){
+				found = true;
+				$scope.keyLinks.splice(link, 1);
+			}
+		}
+
+		if(!found){
+			$scope.keyLinks.push($scope.applications[index]);
+		}
+
+
 	}
 
 
 
 }]);
 
-youottawa.component('keyList', {
-	template: '<div ng-repeat="link in $ctrl.keyLinks" class="ui segment " data-content="{{link.description}}" href="{{url}}">' + 
-				    '<div class="content" >'+
-				     '<a class="header" href="{{link.url}}">{{link.title}}</a>'+
-				       ' </div>'+
-				    '</div>'+
-				  '</div>',
-	controller: function KeyListController(){
-		this.keyLinks = [{
-			"title": "Brightspace",
-			"description": "New Blackboard Learn",
-			"url" : "http://uottawa.brightspace.com"	
-		}]
-	}
-})
 
 
